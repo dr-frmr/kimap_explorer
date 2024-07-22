14dd02e697ff04c856b4625921ab3192ae8dce1f
bitful-pannul <bitful-pancreas@protonmail.com>
2024-07-23 00:43:57 +0300

    hotfix kino account impl address

--- ui/src/abis/index.ts
+++ ui/src/abis/index.ts
@@ -4,8 +4,7 @@ export { fetchNode, fetchNodeInfo, mintFunction, noteFunction } from "./helpers"
 
 export const KINOMAP: `0x${string}` = "0x7290Aa297818d0b9660B2871Bb87f85a3f9B4559";
 export const MULTICALL: `0x${string}` = "0xcA11bde05977b3631167028862bE2a173976CA11";
-// export const KINO_ACCOUNT_IMPL: `0x${string}` = "0x58790D9957ECE58607A4b58308BBD5FE1a2e4789"; //???
-export const KINO_ACCOUNT_IMPL: `0x${string}` = "0x3BA6AE3eca7ca88af8BbCc8A9d8EA5e665b69Eb3"; // .dev and .os?
+export const KINO_ACCOUNT_IMPL: `0x${string}` = "0x58790D9957ECE58607A4b58308BBD5FE1a2e4789";
 export const DOTOS: `0x${string}` = "0xcf0af6048D42B2a8e18b1B062a6c4a027D8C5Ea6";
 export const DOTDEV: `0x${string}` = "0x3BA6AE3eca7ca88af8BbCc8A9d8EA5e665b69Eb3";
 
